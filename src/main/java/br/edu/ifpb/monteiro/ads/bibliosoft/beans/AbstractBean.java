c92df9afeeaf39a582a25b38f72debfc72f77cef
Ricardo <Ricardo@RicardoQbex>
2014-09-17 20:00:53 -0300

    Alter Borrowing in entities

---
+++ src/main/java/br/edu/ifpb/monteiro/ads/bibliosoft/beans/AbstractBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifpb.monteiro.ads.bibliosoft.beans;
+
+import br.edu.ifpb.monteiro.ads.bibliosoft.entities.IdentifiableBiblio;
+import br.edu.ifpb.monteiro.ads.bibliosoft.service.interfaces.InterfaceCrudService;
+
+/**
+ *
+ * @author Ricardo
+ */
+public abstract class AbstractBean {
+    
+    public abstract InterfaceCrudService getService();
+    public abstract IdentifiableBiblio getIdentifiableBiblio();
+    
+    public void save(){
+        getService().save(getIdentifiableBiblio());
+    }
+    
+}
